d0810f8c859a4e086b5655df95d7c74f6fccba2d
aditya25122005 <aditya25pda@gmail.com>
2025-09-29 23:47:05 +0530

    Day-3-task

---
+++ Home_Assignment_3/Number_of_islands.java
@@ -0,0 +1,36 @@
+package Home_Assignment_3;
+
+public class Number_of_islands{
+    public static void main(String[] args) {
+        char[][] grid = {
+    {'1','1','1','1','0'},
+    {'1','1','0','1','0'},
+    {'1','1','0','0','0'},
+    {'0','0','0','0','0'}
+    };
+
+        System.out.println(numIslands(grid));
+    }
+    public static int numIslands(char[][] grid) {
+        int ans=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    Solve(grid,i,j);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void Solve(char[][] arr, int i, int j){
+        if(i<0 || i>=arr.length || j<0 || j>=arr[0].length || arr[i][j]!='1'){
+            return;
+        }
+        arr[i][j]='0';
+        Solve(arr,i+1,j);
+        Solve(arr,i-1,j);
+        Solve(arr,i,j+1);
+        Solve(arr,i,j-1);
+    }
+}
